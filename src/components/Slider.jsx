53ed144fc3da99126a878e0bb5d7dea57a5b154e
unknown <kayv@hotmail.fr>
2024-02-25 20:57:11 +0100

    update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from "react";
-import '../styles/slider.css'
+import React, { useState, useEffect } from 'react';
+import '../styles/slider.css';
 
-export default function Slider({images}){
-    const [currentIndex, setCurrentIndex] = useState(0)
+export default function Slider({ images }) {
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-    function nextSlide(){
-        setCurrentIndex((currentIndex + 1) % images.length);
-    }
+	function nextSlide() {
+		setCurrentIndex((currentIndex + 1) % images.length);
+	}
 
-    // automatic slider
-    useEffect(() => {
-			const timer = setTimeout(() => {
-				nextSlide();
-			}, 2500);
-			return () => clearTimeout(timer);
-		}, [currentIndex]);
+	// Automatic slider
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			nextSlide();
+		}, 2500);
+		return () => clearTimeout(timer);
+	}, [currentIndex]);
 
-    return(
-        <section className="slider">
-            <div className="slider-images">
-        {images.map((image, index) => (
-          <div key={index} className={`slider-image ${index === currentIndex ? 'active' : 'hidden'}`}>
-            <img className="image" src={image} alt="" />
-          </div>
-        ))}
-      </div>
-      {currentIndex >  0 && (
-        <div className="slider-previous">
-          <img src={images[currentIndex -  1]} alt="" />
-        </div>
-      )}
-      {currentIndex < images.length -  1 && (
-        <div className="slider-next">
-          <img src={images[currentIndex +  1]} alt="" />
-        </div>
-      )}
-        </section>
-    )
+	return (
+		<section className="slider">
+			<div className="slider__images">
+				{currentIndex > 0 && (
+					<div className="slider-previous">
+						<img className="image" src={images[currentIndex - 1]} alt="" />
+					</div>
+				)}
+				{images.map((image, index) => (
+					<div
+						key={index}
+						className={`slider-image ${
+							index === currentIndex ? 'active' : 'hidden'
+						}`}
+					>
+						<img className="image" src={image} alt="" />
+					</div>
+				))}
+				{currentIndex < images.length - 1 && (
+					<div className="slider-next">
+						<img className="image" src={images[currentIndex + 1]} alt="" />
+					</div>
+				)}
+			</div>
+		</section>
+	);
 }
